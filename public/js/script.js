db8bc93c344d2f394473303438d8c2c2a23ae8e3
Pavel Popov <pcveykos@gmail.com>
2022-09-15 09:33:49 +0200

    Fix a bug in displaying average score of a game.

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,14 @@ document.addEventListener(
   },
   false
 );
+
+
+const starRating = document.querySelectorAll('.rating-stars')
+for (let ratingBox of starRating) {
+  let rating = ""
+  for (i = 0; i < ratingBox.innerHTML; i++) {
+    rating += "⭐️"
+  }
+  ratingBox.innerHTML = rating
+}
+console.log(starRating)
